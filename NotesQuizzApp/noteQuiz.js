6728c5b6ea2f1784c601cda1617352a81253fe68
mercy-rotich <cherotichm182@gmail.com>
2024-12-27 10:53:46 -0800

    updates

--- NotesQuizzApp/noteQuiz.js
+++ NotesQuizzApp/noteQuiz.js
@@ -2,41 +2,42 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-function showNotes(){
-    notesContainer.innerHTML = localStorage.getItem("notes")
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
 }
 showNotes();
 
-function updateStorage(){
-    localStorage.setItem("notes",notesContainer.innerHTML)
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
-createBtn.addEventListener("click",()=>{
+createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
     img.src = "delete.jpg";
     notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage(); // Update storage after adding a new note
+});
 
-})
-notesContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
         updateStorage();
-    }
-    else if(e.target.tagName === "p"){
+    } else if (e.target.tagName === "P") {
         notes = document.querySelectorAll(".input-box");
-        notes.forEach(nt=>{
-            nt.onkeyup = function(){
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
                 updateStorage();
-            }
-        })
+            };
+        });
     }
-})
-document.addEventListener("keydown",event =>{
-    if(event.key === "Enter"){
+});
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
         document.execCommand("insertLineBreak");
         event.preventDefault();
     }
-})
+});
